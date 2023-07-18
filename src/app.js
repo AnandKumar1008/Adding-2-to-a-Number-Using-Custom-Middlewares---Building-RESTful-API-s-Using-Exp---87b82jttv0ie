7ace902a90877f4cb146b7341ff4d9496f64d329
AnandKumar <anandkumar031199@gmail.com>
2023-07-18 14:30:41 +0530

    add login is done

--- src/app.js
+++ src/app.js
@@ -1,30 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 
-
 //Complete below given Middleware function which adds 2 to a number provided in api as params. Example is shown below
 
-//Example: 
+//Example:
 // localhost:3000/?num=10 --> The router should return { num = 12 }
 
 function add2(req, res, next) {
-
-    //Write Your Code here
-    
+  //Write Your Code here
+  req.sum = parseInt(req.query.num) + 2;
+  next();
 }
 
-app.get('/', add2, (req, res) => {
-    
-    //num should be replaced by num+2 from the get request route
-    const data = {
-        "num" : 5 
-    };
-    
-    res.send(JSON.stringify(data));
+app.get("/", add2, (req, res) => {
+  //num should be replaced by num+2 from the get request route
+  const data = {
+    num: req.sum,
+  };
+
+  res.send(JSON.stringify(data));
 });
-
-
 
 module.exports = app;
